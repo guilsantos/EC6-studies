5214fad10b38df348817de2c3a7872114f956e9d
Skyrior <guiga021@hotmail.com>
2017-10-04 17:19:10 -0300

    Final aula 5

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,7 +6,9 @@ class NegociacaoController{
         this._inputData = $("#data");
         this._inputQuantidade = $("#quantidade");
         this._inputValor = $("#valor");
+
         this._listaNegociacoes = new ListaNegociacoes();
+        this._negociacoesView = new NegociacoesView($("#negociacoesView"));
     }
 
     adiciona(event){
@@ -16,6 +18,8 @@ class NegociacaoController{
         this._limpaFormulario();
 
         console.log(this._listaNegociacoes.negociacoes);
+
+        this._negociacoesView.update(this._listaNegociacoes);
     }
 
     _criaNegociacao(){
