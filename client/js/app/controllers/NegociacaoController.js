af30acc6dde2d92e465481074be6c794c41940e7
skyrior <guiga021@hotmail.com>
2017-10-06 00:34:53 -0300

    2.1 completo

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -24,8 +24,6 @@ class NegociacaoController{
 
         this._listaNegociacoes.adiciona(this._criaNegociacao());
 
-        this._negociacoesView.update(this._listaNegociacoes);
-        
         this._mensagem.texto = "Negociação adicionada com sucesso";
         this._mensagemView.update(this._mensagem);
 
@@ -34,7 +32,6 @@ class NegociacaoController{
 
     apaga(){
         this._listaNegociacoes.esvazia();
-        this._negociacoesView.update(this._listaNegociacoes);
 
         this._mensagem.texto = "Negociações apagadas com sucesso";
         this._mensagemView.update(this._mensagem);
